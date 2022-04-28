31adda1104a1d1c154be5cd5c11f295d2d15eafa
Hung <yung515253@gmail.com>
2022-04-28 10:34:53 +0700

    -commit

---
+++ app/src/main/java/com/hungphamcom/mellowcourse/add_new_item.java
@@ -0,0 +1,14 @@
+package com.hungphamcom.mellowcourse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class add_new_item extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_item);
+    }
+}
